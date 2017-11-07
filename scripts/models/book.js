5a45e59133bb11c8276b9cdbf421f6a8d6aa6931
strawbee <yuejiao.hou@gmail.com>
2017-11-07 13:25:36 -0800

    git told me to

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -3,12 +3,12 @@
 var app = app || {};
 var __API_URL__ = 'https://ncjh-booklist.herokuapp.com';
 
-// function fetchAll = callback => {
-//   $.get('/api/v1/books')
-//     .then(results => {
-//       console.log(results);
-//       callback();
-//     })
-// };
+function fetchAll() {
+  $.get('/api/v1/books')
+    .then(results => {
+      console.log(results);
+    });
+}
 
 console.log('test');
+fetchAll();

72ec4f94501700c19b529a34621afbc9e7e16139
strawbee <yuejiao.hou@gmail.com>
2017-11-07 12:21:59 -0800

    trying to fix things

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -3,12 +3,12 @@
 var app = app || {};
 var __API_URL__ = 'https://ncjh-booklist.herokuapp.com';
 
-function fetchAll = callback => {
-  $.get('/api/v1/books')
-    .then(results => {
-      console.log(results);
-      callback();
-    })
-};
+// function fetchAll = callback => {
+//   $.get('/api/v1/books')
+//     .then(results => {
+//       console.log(results);
+//       callback();
+//     })
+// };
 
 console.log('test');

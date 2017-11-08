0ed14f26c0737a67a88680a611796d09813468d4
strawbee <yuejiao.hou@gmail.com>
2017-11-07 16:36:33 -0800

    fleshing out book.js

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -5,22 +5,48 @@ var __API_URL__ = 'https://ncjh-booklist.herokuapp.com';
 
 // (function(module){
 
-function errorCallback(err){
+  /* What is this function doing? */
+  function errorCallback(err) {
+    console.error(err);
+    module.errorView.initErrorPage(err);
+  }
 
-}
+  // Declaring a Constructor function that assigns properties of Book objects
+  function Book(rawDataObj) {
+    Object.keys(rawDataObj).forEach(key => this[key] = rawDataObj[key]);
+  }
 
-//this pulls anything
-function fetchAll(callback){
-  $.get(`${__API_URL__}/api/v1/books`)
-    .then(results => {
-      console.log(results);
-      // callback();
-    })
-}
-fetchAll();
-// Book.all = [];
-// Book.loadAll = rows => Book.all = rows.sort(())
+  // Array of all Book objects
+  Book.all = [];
 
-// app.Book = Book;
-console.log('test');
-// })()
+  // Renders all of the Book objects to HTML using the Handlebars template in index.html
+  Book.prototype.toHtml = function() {
+    var template = Handlebars.compile($('#book-template').text());
+    return template(this);
+  }
+
+  /* Why does toHtml have a protoype and loadAll doesn't again? */
+  Book.loadAll => rawData => {
+    /* What are we sorting by? */
+    // rawData.sort((a, b) => )
+
+    // Populating into the Book array each object in the rawData file
+    Book.all = rawData.map(bookObject => new Book(bookObject));
+  }
+
+  // Grabs the raw data from our server, which communicates with our database to load the data from SQL tables. If successful, instantiates each result via the loadAll method into our Book constructor and loads it into our Book.all array.
+  Book.fetchAll = callback => {
+    $.get(`${__API_URL__}/api/v1/books`)
+      .then (results => {
+        Book.loadAll(results);
+        if (callback) callback();
+      })
+      .catch (errorCallback);
+  }
+
+
+
+//   module.Book = Book;
+// })(app);
+
+Book.fetchAll();

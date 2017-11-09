0c7baa06628001e1473ddc7ecdd9cdabeb4527ce
Nicholas Carignan <nick.carignan@sbcglobal.net>
2017-11-08 19:30:27 -0800

    fixing pagejs

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -7,6 +7,7 @@ page();
 
 
 //adding click functions here
+<<<<<<< HEAD
 // $('book-main').click(()=>{
 //   app.Book.fetchOne(app.bookView.initDetailPage)
 // })
@@ -27,3 +28,24 @@ page();
 //   app.Book.fetchOne(app.bookView.initDetailPage)
 // })
 
+=======
+$('book-main').click(()=>{
+  app.Book.fetchOne(app.bookView.initDetailPage)
+})
+
+$('#Book1').click(()=>{
+  app.Book.fetchOne(app.bookView.initDetailPage)
+})
+$('#Book2').click(()=>{
+  app.Book.fetchOne(app.bookView.initDetailPage)
+})
+$('#Book3').click(()=>{
+  app.Book.fetchOne(app.bookView.initDetailPage)
+})
+$('#Book4').click(()=>{
+  app.Book.fetchOne(app.bookView.initDetailPage)
+})
+$('#Book5').click(()=>{
+  app.Book.fetchOne(app.bookView.initDetailPage)
+})
+>>>>>>> ff7044931e23870afd5dd7b92c4522849ee4acdf

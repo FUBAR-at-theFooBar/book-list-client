fd0078eab4a643c4818e5dd06ba499b7ac8a5709
Nicholas Carignan <nick.carignan@sbcglobal.net>
2017-11-10 15:20:42 -0800

    removed extraneous pagejs functionality

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -6,8 +6,9 @@ page('/books/error', ctx => app.bookView.errorCallback(ctx));
 
 page('/books/new', ctx => app.bookView.initFormPage(ctx));
 
+//we incindentally wrote more complex buttons instead of pagejs functions for these but we left them in to show we understood the concept.
 // page('/books/:book_id', ctx => app.Book.fetchOne(ctx, app.bookView.initDetailPage));
 //
-// page('/books/update/:book_id', ctx => app.Book.update(ctx, app.bookView.initUpdatePage));
+// page('/books/:book_id/update', ctx => initUpdatePage(ctx));
 
 page();

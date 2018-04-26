891c8301f11875f4f96b6b9832e074e106df3b04
Nicholas Carignan <nick.carignan@sbcglobal.net>
2018-04-26 14:32:45 -0700

    try try again

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -1,10 +1,10 @@
 'use strict'
 
-page('/', ctx => app.Book.fetchAll(app.bookView.initIndexPage));
+page('/book-list-client/', ctx => app.Book.fetchAll(app.bookView.initIndexPage));
 
-page('/books/error', ctx => app.bookView.errorCallback(ctx));
+page('/book-list-client/books/error', ctx => app.bookView.errorCallback(ctx));
 
-page('/books/new', ctx => app.bookView.initFormPage(ctx));
+page('/book-list-client/books/new', ctx => app.bookView.initFormPage(ctx));
 
 //we incindentally wrote more complex buttons instead of pagejs functions for these but we left them in to show we understood the concept.
 // page('/books/:book_id', ctx => app.Book.fetchOne(ctx, app.bookView.initDetailPage));

3229fa3c8936a9541fce52ae13ec07a62a5725be
Nicholas Carignan <nick.carignan@sbcglobal.net>
2018-04-25 17:36:56 -0700

    reset to heroku route

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var app = app || {};
-// var __API_URL__ = 'https://ncjh-booklist.herokuapp.com';
-var __API_URL__ = 'http://localhost:3000';
+var __API_URL__ = 'https://ncjh-booklist.herokuapp.com';
+// var __API_URL__ = 'http://localhost:3000';
 (function(module){
 
   Book.TOKEN = '666';
